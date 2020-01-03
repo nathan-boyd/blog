8c20c0b8e5c812fe38034cdc1f6e85ee765a86ca
Nathan Boyd <nboyd@salesforce.com>
2020-01-03 13:17:07 -0500

    feat(tests): add run local script

---
+++ scripts/run-local.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+export JEKYLL_VERSION=3.5
+
+if [ ! "$(docker ps -q -f name=blog)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=blog)" ]; then
+        docker rm blog > /dev/null
+    fi
+fi
+
+docker run --name blog --volume="$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll:$JEKYLL_VERSION jekyll serve --watch --drafts
