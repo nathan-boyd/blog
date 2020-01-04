475d897acc752f954df34cde5c2daf4f6440f86f
Nathan Boyd <nboyd@salesforce.com>
2020-01-04 10:53:19 -0500

    feat(dev): update run local script to use gem cache

--- scripts/run-local.sh
+++ scripts/run-local.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-export JEKYLL_VERSION=3.5
+export JEKYLL_VERSION=3.8
 
 if [ ! "$(docker ps -q -f name=blog)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=blog)" ]; then
@@ -8,4 +8,9 @@ if [ ! "$(docker ps -q -f name=blog)" ]; then
     fi
 fi
 
-docker run --name blog --volume="$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll:$JEKYLL_VERSION jekyll serve --watch --drafts
+docker run --name blog \
+    --volume="$PWD/vendor/bundle:/usr/local/bundle" \
+    --volume="$PWD:/srv/jekyll" \
+    -p 4000:4000 -it \
+    jekyll/jekyll:$JEKYLL_VERSION \
+    jekyll serve --watch --drafts
